7f580704f833ce91858b3f7d25013e1e7daea031
stanislas jouven <stanislasjouven@gmail.com>
2020-04-30 16:25:47 +0200

    created map

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
 
 class MapPlot {
 
-	makeColorbar(svg, color_scale, top_left, colorbar_size, scaleClass=d3.scaleLog) {
-
-
-
 	constructor(svg_element_id) {
 		this.svg = d3.select('#' + svg_element_id);
 
+		// may be useful for calculating scales
+		const svg_viewbox = this.svg.node().viewBox.animVal;
+		this.svg_width = svg_viewbox.width;
+		this.svg_height = svg_viewbox.height;
+
+		//Define map projection
+		var projection = d3.geoMercator()
+								 .translate([this.svg_width/2, this.svg_height/2])
+								 .scale([this.svg_width * 0.16]);
+
+		//Define path generator
+		var path = d3.geoPath()
+						 .projection(projection);
+
+		//this.map_container = this.svg.append('g');
+
+		const map_promise = d3.json("data/countries.json").then((topojson_raw) => {
+			const country_paths = topojson.feature(topojson_raw, topojson_raw.objects.countries);
+			return country_paths.features;
+		});
+
+		Promise.all([map_promise]).then((results) => {
+			let map_data = results[0];
+			this.svg.selectAll("path")
+ 				 .data(map_data)
+ 				 .enter()
+ 				 .append("path")
+ 				 .attr("d", path)
+ 				 .style("fill", "steelblue");
+
+		});
 
 
-		// D3 Projection
-		// similar to scales
-		const projection = d3.Patterson()
+	}
 
-
-		// path generator to convert JSON to SVG paths
-		const path_generator = d3.geoPath()
-			.projection(projection);
-
+}
 
 
 function whenDocumentLoaded(action) {
