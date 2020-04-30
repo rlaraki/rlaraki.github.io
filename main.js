45336ee1852ec5776087a628de4f97f9c71b8262
Olivier Lam <olivier.lam@epfl.ch>
2020-04-30 16:33:17 +0200

    commit pour beurre

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
 
 class MapPlot {
 
-	makeColorbar(svg, color_scale, top_left, colorbar_size, scaleClass=d3.scaleLog) {
-
 
 
 	constructor(svg_element_id) {
 		this.svg = d3.select('#' + svg_element_id);
 
-
+		// may be useful for calculating scales
+		const svg_viewbox = this.svg.node().viewBox.animVal;
+		this.svg_width = svg_viewbox.width;
+		this.svg_height = svg_viewbox.height;
 
 		// D3 Projection
 		// similar to scales
-		const projection = d3.Patterson()
-
+		const projection = d3.geoNaturalEarth1()
+			.rotate([0, 0])
+			.center([8.3, 46.8]) // WorldSpace: Latitude and longitude of center of switzerland
+			.scale(13000)
+			.translate([this.svg_width / 2, this.svg_height / 2]) // SVG space
+			.precision(.1);
 
 		// path generator to convert JSON to SVG paths
 		const path_generator = d3.geoPath()
 			.projection(projection);
 
+		const map_promise = d3.json("data/countries-50m.json").then((topojson_raw) => {
+			const canton_paths = topojson.feature(topojson_raw, topojson_raw.objects.countries);
+			return canton_paths.features;
+		});
+
+		Promise.all([map_promise]).then((results)) => {
+			let map_data = results[0]
+
+			
+		}
+
+	}
+}
 
 
 function whenDocumentLoaded(action) {
