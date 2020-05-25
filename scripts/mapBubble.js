a21ef21acfb9a75c7d8b1eedbf6e0e0995171acc
stanislas jouven <stanislasjouven@gmail.com>
2020-05-25 16:31:00 +0200

    updates mapBubble

--- scripts/mapBubble.js
+++ scripts/mapBubble.js
@@ -4,7 +4,7 @@ class MapBubble extends MapPlot {
 		super();
 	}
 
-  draw(){
+  draw(date_indice = 0){
 
 		var map_container_svg = document.getElementById('map_container_svg')
 		this.svg_viewbox = this.svg.node().viewBox.animVal;
@@ -50,26 +50,57 @@ class MapBubble extends MapPlot {
 
 			let current_plot = this;
 
+      var format = d3.format(",");
 			var pred;
-			var countryShapes = this.map_container.selectAll("path")
+      let value = [dates[0], 0]
+      var pred;
+			var pred_opacity;
+      var pred_stroke_color;
+
+      this.map_container.selectAll("path")
 				 .data(map_data)
 				 .enter()
 				 .append("path")
-				 .on("mouseover",function(){
-					 pred = this.style.fill;
-					 d3.select(this).style('fill', 'red').style('stroke');
+				 .on("mouseover",function(d){
+					 pred = this.style.stroke_width;
+           pred_opacity = this.style.opacity;
+           pred_stroke_color = this.style.stroke;
+           if (this.style.stroke != 'black'){
+             d3.select(this)
+                .style('stroke', 'white')
+                .style('stroke-width', 1)
+                .style('opacity', 1.2);
+           } else {
+             d3.select(this)
+                .style('stroke', 'black')
+                .style('stroke-width', 1)
+                .style('opacity', 1.2);
+           }
+
 				  	})
- 	          .on("mouseout",function(){
-		 					d3.select(this).style('fill', pred);})
-							.on("click", function(){
-								if (this.style.stroke != 'red'){
-								d3.select(this).style('stroke', 'red');
-							} else {
-								d3.select(this).style('stroke', null);
-							}
-							})
-						.attr('fill', 'black')
-								.attr("d", path);
+          .on("mousemove", function(d) {
+            var coordinates = d3.mouse(current_plot.svg.node());
+            current_plot.tooltip.classed('hidden', false)
+             .attr('style', 'left:' + (coordinates[0] + 20) + 'px; top:' + coordinates[1] + 'px')
+             .html("<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Stringency Index: </strong><span class='details'>" + console.log(d) +"</span>");
+          })
+          .on("mouseout",function(d){
+            current_plot.tooltip.classed('hidden', true);
+  					d3.select(this).style('stroke-width', 1)
+              .style('opacity', pred_opacity)
+              .style('stroke', pred_stroke_color);})
+  				.on("click", function(){
+  					if (this.style.stroke != 'black'){
+  					  d3.select(this).style('stroke', 'black')
+                .style('stroke-width', 1);
+  				} else {
+  					d3.select(this).style('stroke', null);
+  				}
+          pred_stroke_color = this.style.stroke;
+  				})
+					.attr('fill', 'black')
+					.attr("d", path)
+          .style("fill", "#ccc");
 
 
 			 d3.select("#slider")
@@ -87,15 +118,17 @@ class MapBubble extends MapPlot {
 				.attr("displayValue", true)
 	 			.on("input", function() {
 	 				var date = dates[this.value];
+          value = [date, this.value];
 					var point = document.getElementById('point_svg');
 					if (point)
 						point.remove();
 					point_container = current_plot.svg.append("g").attr('id', 'point_svg');
-	 				current_plot.drawData(countryShapes, date, this.value, data, projection, point_container);
+          current_plot.date_ind = this.value;
+	 				current_plot.drawData(date, this.value, data, projection, point_container);
 	 			});
 
 			var point_container = this.svg.append("g").attr('id', 'point_svg');
-			this.drawData(countryShapes, dates[0], 0, data, projection, point_container);
+			this.drawData(dates[date_indice], date_indice, data, projection, point_container);
 
 			const zoom = d3.zoom()
 	 	 					       .scaleExtent([1, 8])
@@ -103,24 +136,28 @@ class MapBubble extends MapPlot {
 	 										  d3.event.transform.x = Math.min(0, Math.max(d3.event.transform.x, width - width * d3.event.transform.k));
 	 						   				d3.event.transform.y = Math.min(0, Math.max(d3.event.transform.y, height - height * d3.event.transform.k));
 	 											current_plot.map_container.selectAll('path').attr("transform", d3.event.transform);
-												point_container.selectAll('circle').attr('transform', d3.event.transform.toString());
+												point_container.selectAll('circle').attr("transform", d3.event.transform);
+
+
 	 	 								 });
 	 			this.svg.call(zoom);
-        this.map_container.append("g")
+        this.legend = this.map_container.append("g")
           .attr("fill", "#777")
           .attr("transform", "translate(" + width * 0.05 + ', ' + height * 0.95 + ")")
           .attr("text-anchor", "middle")
           .style("font", "10px sans-serif")
           .selectAll("circle")
             .data([parseInt(this.max/4), parseInt(this.max/2), parseInt(this.max)])
-            .enter()
-            .append("circle")
+            .enter();
+        this.legend.append("circle")
       	        .attr("fill", "none")
       	        .attr("stroke", "black")
       	        .attr("cy", d => -this.point_scale(d))
       	        .attr("r", this.point_scale);
+        this.legend.append("text")
+  	        .attr("y", d => -2 * this.point_scale(d))
+  	        .attr("dy", "1.3em")
+  	        .text(d3.format(".1s"));
 		});
 	}
-
-
 }
