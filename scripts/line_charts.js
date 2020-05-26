eac50f454b66469dca3e46d75c16bf61f452efd5
Olivier Lam <olivier.lam@epfl.ch>
2020-05-26 21:12:36 +0100

    commit for my bgs

--- scripts/line_charts.js
+++ scripts/line_charts.js
@@ -1,49 +1,17 @@
-var data = [{
-    name: "USA",
-    values: [
-      {date: "2000", price: "100"},
-      {date: "2001", price: "110"},
-      {date: "2002", price: "145"},
-      {date: "2003", price: "241"},
-      {date: "2004", price: "101"},
-      {date: "2005", price: "90"},
-      {date: "2006", price: "10"},
-      {date: "2007", price: "35"},
-      {date: "2008", price: "21"},
-      {date: "2009", price: "201"}
-    ]
-  },
-  {
-    name: "Canada",
-    values: [
-      {date: "2000", price: "200"},
-      {date: "2001", price: "120"},
-      {date: "2002", price: "33"},
-      {date: "2003", price: "21"},
-      {date: "2004", price: "51"},
-      {date: "2005", price: "190"},
-      {date: "2006", price: "120"},
-      {date: "2007", price: "85"},
-      {date: "2008", price: "221"},
-      {date: "2009", price: "101"}
-    ]
-  },
-  {
-    name: "Maxico",
-    values: [
-      {date: "2000", price: "50"},
-      {date: "2001", price: "10"},
-      {date: "2002", price: "5"},
-      {date: "2003", price: "71"},
-      {date: "2004", price: "20"},
-      {date: "2005", price: "9"},
-      {date: "2006", price: "220"},
-      {date: "2007", price: "235"},
-      {date: "2008", price: "61"},
-      {date: "2009", price: "10"}
-    ]
-  }];
+var data = [];
 
+var request = new XMLHttpRequest();
+request.open('GET', '../data/general_data.json', false);
+request.send(null)
+
+var casesData = JSON.parse(request.responseText)
+//list_countries.forEach(add_data);
+/*
+function add_data(item) {
+  data = data.concat(casesData[item])
+}
+*/
+data = [casesData['Switzerland'], casesData['France']];
 
 var width = 420;
 var height = 300;
@@ -63,23 +31,8 @@ var circleRadiusHover = 6;
 
 
 /* Format Data */
-var parseDate = d3.timeParse("%Y");
-data.forEach(function(d) {
-  d.values.forEach(function(d) {
-    d.date = parseDate(d.date);
-    d.price = +d.price;
-  });
-});
+var parseDate = d3.timeParse("%d-%m-%Y");
 
-
-/* Scale */
-var xScale = d3.scaleTime()
-  .domain(d3.extent(data[0].values, d => d.date))
-  .range([0, width-margin]);
-
-var yScale = d3.scaleLinear()
-  .domain([0, d3.max(data[0].values, d => d.price)])
-  .range([height-margin, 0]);
 
 /* Add Axis into SVG */
 
@@ -88,137 +41,157 @@ var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 /* Add SVG */
 var svg = d3.select("div#right_scroll_plot").append("svg")
+  .attr("id", 'line_chart')
+  .on("click", function (d) {
+    data = []
+    list_countries.forEach((item) => {
+      data = data.concat(casesData[item])
+    });
+    console.log('line charts list countries', list_countries);
+    console.log('line charts data', data);
+    draw()
+  })
   .attr("width", (width+margin)+"px")
   .attr("height", (height+margin)+"px")
   .append('g')
   .attr("transform", `translate(${margin}, ${margin})`);
 
-var xAxis = d3.axisBottom(xScale).ticks(5);
-var yAxis = d3.axisLeft(yScale).ticks(5);
 
 
-/* Add line into SVG */
-var line = d3.line()
-  .x(d => xScale(d.date))
-  .y(d => yScale(d.price));
+function draw() {
 
-let lines = svg.append('g')
-  .attr("clip-path", "url(#clip)")
-  .attr('class', 'lines');
-
-
-lines.selectAll('.line-group')
-  .data(data).enter()
-  .append('g')
-  .attr('class', 'line-group')
-  .on("mouseover", function(d, i) {
-      svg.append("text")
-        .attr("class", "title-text")
-        .style("fill", color(i))
-        .text(d.name)
-        .attr("text-anchor", "middle")
-        .attr("x", (width-margin)/2)
-        .attr("y", 5);
-    })
-  .on("mouseout", function(d) {
-      svg.select(".title-text").remove();
-    })
-  .append('path')
-  .attr('class', 'line')
-  .attr('d', d => line(d.values))
-  .style('stroke', (d, i) => color(i))
-  .style('opacity', lineOpacity)
-  .on("mouseover", function(d) {
-      d3.selectAll('.line')
-					.style('opacity', otherLinesOpacityHover);
-      d3.selectAll('.circle')
-					.style('opacity', circleOpacityOnLineHover);
-      d3.select(this)
-        .style('opacity', lineOpacityHover)
-        .style("stroke-width", lineStrokeHover)
-        .style("cursor", "pointer");
-    })
-  .on("mouseout", function(d) {
-      d3.selectAll(".line")
-					.style('opacity', lineOpacity);
-      d3.selectAll('.circle')
-					.style('opacity', circleOpacity);
-      d3.select(this)
-        .style("stroke-width", lineStroke)
-        .style("cursor", "none");
+  data.forEach(function(d) {
+    d.values.forEach(function(d) {
+      d.date = parseDate(d.date);
+      d.cases = +d.cases;
     });
+  });
 
 
-/* Add circles in the line */
-lines.selectAll("circle-group")
-  .data(data).enter()
-  .append("g")
-  .style("fill", (d, i) => color(i))
-  .selectAll("circle")
-  .data(d => d.values).enter()
-  .append("g")
-  .attr("class", "circle")
-  .on("mouseover", function(d) {
-      d3.select(this)
-        .style("cursor", "pointer")
-        .append("text")
-        .attr("class", "text")
-        .text(`${d.price}`)
-        .attr("x", d => xScale(d.date) + 5)
-        .attr("y", d => yScale(d.price) - 10);
-    })
-  .on("mouseout", function(d) {
-      d3.select(this)
-        .style("cursor", "none")
-        .transition()
-        .duration(duration)
-        .selectAll(".text").remove();
-    })
-  .append("circle")
-  .attr("cx", d => xScale(d.date))
-  .attr("cy", d => yScale(d.price))
-  .attr("r", circleRadius)
-  .style('opacity', circleOpacity)
-  .on("mouseover", function(d) {
-        d3.select(this)
-          .transition()
-          .duration(duration)
-          .attr("r", circleRadiusHover);
+  /* Scale */
+  var xScale = d3.scaleTime()
+    .domain(d3.extent(data[0].values, d => d.date))
+    .range([0, width-margin]);
+
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(data[0].values, d => d.cases)])
+    .range([height-margin, 0]);
+
+  var xAxis = d3.axisBottom(xScale).ticks(10);
+  var yAxis = d3.axisLeft(yScale).ticks(10);
+
+  /* Add line into SVG */
+  var line = d3.line()
+    .x(d => xScale(d.date))
+    .y(d => yScale(d.cases));
+
+  let lines = svg.append('g')
+    .attr('class', 'lines');
+
+
+  lines.selectAll('.line-group')
+    .data(data).enter()
+    .append('g')
+    .attr('class', 'line-group')
+    .on("mouseover", function(d, i) {
+        svg.append("text")
+          .attr("class", "title-text")
+          .style("fill", color(i))
+          .text(d.name)
+          .attr("text-anchor", "middle")
+          .attr("x", (width-margin)/2)
+          .attr("y", 5);
       })
     .on("mouseout", function(d) {
+        svg.select(".title-text").remove();
+      })
+    .append('path')
+    .attr('class', 'line')
+    .attr('d', d => line(d.values))
+    .style('stroke', (d, i) => color(i))
+    .style('opacity', lineOpacity)
+    .on("mouseover", function(d) {
+        d3.selectAll('.line')
+  					.style('opacity', otherLinesOpacityHover);
+        d3.selectAll('.circle')
+  					.style('opacity', circleOpacityOnLineHover);
         d3.select(this)
-          .transition()
-          .duration(duration)
-          .attr("r", circleRadius);
+          .style('opacity', lineOpacityHover)
+          .style("stroke-width", lineStrokeHover)
+          .style("cursor", "pointer");
+      })
+    .on("mouseout", function(d) {
+        d3.selectAll(".line")
+  					.style('opacity', lineOpacity);
+        d3.selectAll('.circle')
+  					.style('opacity', circleOpacity);
+        d3.select(this)
+          .style("stroke-width", lineStroke)
+          .style("cursor", "none");
       });
 
 
+  /* Add circles in the line */
+  lines.selectAll("circle-group")
+    .data(data).enter()
+    .append("g")
+    .style("fill", (d, i) => color(i))
+    .selectAll("circle")
+    .data(d => d.values).enter()
+    .append("g")
+    .attr("class", "circle")
+    .on("mouseover", function(d) {
+        d3.select(this)
+          .style("cursor", "pointer")
+          .append("text")
+          .attr("class", "text")
+          .text(`${d.cases}`)
+          .attr("x", d => xScale(d.date) + 5)
+          .attr("y", d => yScale(d.cases) - 10);
+      })
+    .on("mouseout", function(d) {
+        d3.select(this)
+          .style("cursor", "none")
+          .transition()
+          .duration(duration)
+          .selectAll(".text").remove();
+      })
+    .append("circle")
+    .attr("cx", d => xScale(d.date))
+    .attr("cy", d => yScale(d.cases))
+    .attr("r", circleRadius)
+    .style('opacity', circleOpacity)
+    .on("mouseover", function(d) {
+          d3.select(this)
+            .transition()
+            .duration(duration)
+            .attr("r", circleRadiusHover);
+        })
+      .on("mouseout", function(d) {
+          d3.select(this)
+            .transition()
+            .duration(duration)
+            .attr("r", circleRadius);
+        });
 
 
-svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", `translate(0, ${height-margin})`)
-  .call(xAxis);
-
-svg.append("g")
-  .attr("class", "y axis")
-  .call(yAxis)
-  .append('text')
-  .attr("y", 15)
-  .attr("transform", "rotate(-90)")
-  .attr("fill", "#000")
-  .text("Total values");
 
 
-// If user double click, reinitialize the chart
-svg.on("dblclick",function(){
-  x.domain(d3.extent(data, function(d) { return d.date; }))
-  xAxis.transition().call(d3.axisBottom(x))
-  line
-    .select('.line')
-    .transition()
-    .attr("d", d3.line()
-      .x(function(d) { return x(d.date) })
-      .y(function(d) { return y(d.value) })
-  )
-});
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0, ${height-margin})`)
+    .call(xAxis);
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append('text')
+    .attr("y", 15)
+    .attr("transform", "rotate(-90)")
+    .attr("fill", "#000")
+    .text("Total values");
+
+
+}
+
+draw()
